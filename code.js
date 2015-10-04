7a2150d0229665a328cc05d27de8f2c00454bdc6
Hassan Salehe Matar <hmatar@ku.edu.tr>
2015-10-04 11:01:35 +0300

    First pages commit

--- code.js
+++ code.js
@@ -31,38 +31,6 @@ var cy = cytoscape({
         'line-color': '#ffaaaa',
         'target-arrow-color': '#ffaaaa',
 		'content': 'data(label)'
-      })
-    .selector('#bird')
-      .css({
-        'background-image': 'https://farm8.staticflickr.com/7272/7633179468_3e19e45a0c_b.jpg'
-      })
-    .selector('#cat')
-      .css({
-        'background-image': 'https://farm2.staticflickr.com/1261/1413379559_412a540d29_b.jpg'
-      })
-    .selector('#ladybug')
-      .css({
-        'background-image': 'https://farm4.staticflickr.com/3063/2751740612_af11fb090b_b.jpg'
-      })
-  .selector('#aphid')
-      .css({
-        'background-image': 'https://farm9.staticflickr.com/8316/8003798443_32d01257c8_b.jpg'
-      })
-  .selector('#rose')
-      .css({
-        'background-image': 'https://farm6.staticflickr.com/5109/5817854163_eaccd688f5_b.jpg'
-      })
-  .selector('#grasshopper')
-      .css({
-        'background-image': 'https://farm7.staticflickr.com/6098/6224655456_f4c3c98589_b.jpg'
-      })
-  .selector('#plant')
-      .css({
-        'background-image': 'https://farm1.staticflickr.com/231/524893064_f49a4d1d10_z.jpg'
-      })
-  .selector('#wheat')
-      .css({
-        'background-image': 'https://farm3.staticflickr.com/2660/3715569167_7e978e8319_b.jpg'
       }),
   
   elements: {
@@ -165,7 +133,7 @@ cy.on('tap', 'node', function(){
     }, {
       duration: duration,
       complete: function(){
-        thisFood.remove();
+        //thisFood.remove();
       }
     });
     
